452165ba754efb66e657184cae714961cf955f90
Jchips <20979096+Jchips@users.noreply.github.com>
2025-03-17 05:14:21 -0700

    passing all given tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -4,17 +4,17 @@ def read_template(file_path):
   with open(file_path) as file:
     return file.read()
 
-file_contents = read_template('assets/dark_and_stormy_night_template.txt')
-print(file_contents)
-
 def parse_template(file_contents):
-  placeHolders = re.findall('\\{(\\w+)\\}', file_contents)
-  extracted_str = re.sub('\{(\w+)\}', '{}', file_contents)
-  print(placeHolders)
-  print(extracted_str)
+  placeHolders = re.findall(r'\{([^}]+)\}', file_contents)
+  extracted_str = re.sub(r'\{([^}]+)\}', '{}', file_contents)
   return extracted_str, tuple(placeHolders)
 
-parse_template(file_contents)
+def input_values(placeHolders):
+  res_list = []
+  for word_class in placeHolders:
+    res_list.append(input(f'{word_class}? '))
+  return tuple(res_list)
 
-def merge(arg):
-  pass
+def merge(extracted_str, user_responses):
+  result = extracted_str.format(*user_responses)
+  return result
--- madlib_cli/main.py
+++ madlib_cli/main.py
@@ -1,5 +1,18 @@
-print('This game is called Madlib.\n')
-print("""You will be given a Madlib template.
-To fill it out, enter whatever type of word the program asks (adjective, noun, verb, etc.).
-Once finished, the full story (Madlib) you have created will display.\n""")
+from madlib import read_template, parse_template, input_values, merge
+
+print('This game is called Mad Libs.\n')
+print('Instructions:')
+print('''When asked for a word class (adjective, noun, verb, etc), type out
+any word that fits the category.\n
+For example, you might type \"tall\" when asked \"Adjective?\"\n
+Once finished, the full story that is created will display.\n''')
 print('Have fun!\n')
+
+print("\n=== Game start ===\n")
+
+file_contents = read_template('assets/dark_and_stormy_night_template.txt')
+extracted_str, placeHolders = parse_template(file_contents)
+user_responses = input_values(placeHolders)
+
+print("\n=== Finished Mad Lib ===\n")
+print(merge(extracted_str, user_responses))
