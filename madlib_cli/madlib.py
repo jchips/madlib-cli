1ab0d1dc44d48a5de02ff19385fce38cadec8137
Jchips <20979096+Jchips@users.noreply.github.com>
2025-03-17 05:48:22 -0700

    writes completed madlib to new file

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -5,16 +5,21 @@ def read_template(file_path):
     return file.read()
 
 def parse_template(file_contents):
-  placeHolders = re.findall(r'\{([^}]+)\}', file_contents)
-  extracted_str = re.sub(r'\{([^}]+)\}', '{}', file_contents)
+  match_regex = r"\{([^}]+)\}"
+  placeHolders = re.findall(f"{match_regex}", file_contents)
+  extracted_str = re.sub(f"{match_regex}", "{}", file_contents)
   return extracted_str, tuple(placeHolders)
 
 def input_values(placeHolders):
   res_list = []
   for word_class in placeHolders:
-    res_list.append(input(f'{word_class}? '))
+    res_list.append(input(f"{word_class}? "))
   return tuple(res_list)
 
 def merge(extracted_str, user_responses):
   result = extracted_str.format(*user_responses)
   return result
+
+def write_madlib(file_name, result):
+  with open(f"{file_name}.txt", "w") as file:
+    file.write(result)
--- madlib_cli/main.py
+++ madlib_cli/main.py
@@ -1,18 +1,25 @@
-from madlib import read_template, parse_template, input_values, merge
+from madlib import read_template, parse_template, input_values, merge, write_madlib
 
-print('This game is called Mad Libs.\n')
-print('Instructions:')
-print('''When asked for a word class (adjective, noun, verb, etc), type out
+print("This game is called Mad Libs.\n")
+print("Instructions:")
+print("""When asked for a word class (adjective, noun, verb, etc), type out
 any word that fits the category.\n
 For example, you might type \"tall\" when asked \"Adjective?\"\n
-Once finished, the full story that is created will display.\n''')
-print('Have fun!\n')
+Once finished, the full story that is created will display.\n""")
+print("Have fun!\n")
 
 print("\n=== Game start ===\n")
 
-file_contents = read_template('assets/dark_and_stormy_night_template.txt')
+file_contents = read_template("assets/dark_and_stormy_night_template.txt")
 extracted_str, placeHolders = parse_template(file_contents)
 user_responses = input_values(placeHolders)
 
 print("\n=== Finished Mad Lib ===\n")
-print(merge(extracted_str, user_responses))
+result = merge(extracted_str, user_responses)
+print(result)
+
+save = input("\nWould you like to save your finished Mad Lib? y/n: ")
+
+if save.lower() == "y" or save.lower() == "yes":
+  file_name = input("\nChoose a file name to save your madlib result: ")
+  write_madlib(file_name, result)
